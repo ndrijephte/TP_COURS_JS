c826b76f0603389c5bc6819c237b018bf6fd23c3
ndrijephte <ndrijephtekoffi@gmail.com>
2024-01-23 11:26:55 +0000

    Fin du TP 1 du Cours

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
+
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// });
+
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
